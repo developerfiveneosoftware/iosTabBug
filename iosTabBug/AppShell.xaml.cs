f02f8db212b4ceb63abbe071e04c87d06535a8b9
developerfiveneosoftware <developerfive@neosoftware.uk>
2023-02-07 14:59:18 +0000

    Add project files.

---
+++ iosTabBug/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using iosTabBug.ViewModels;
+using iosTabBug.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace iosTabBug
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ iosTabBug/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using iosTabBug.Models;
+using iosTabBug.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iosTabBug.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
